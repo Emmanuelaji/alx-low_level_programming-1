ed96694176a3c2c190473da7f7d0b8de2e0bcc8b
Chetachi <maduchetat231@gmail.com>
2022-07-01 07:52:10 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:Always 0(Success)
+ */
+int main(void)
+{
+int i;
+char l;
+i = 48;
+while  (i < 58)
+{
+putchar(i);
+i++;
+}
+l = 'a';
+while (l <= 'f')
+{
+putchar(l);
+l++;
+}
+putchar('\n');
+return (0);
+}
