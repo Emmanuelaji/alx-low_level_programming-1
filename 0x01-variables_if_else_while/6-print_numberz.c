04cf12a87382c5fc498b434185d23f8262ddf782
Chetachi <maduchetat231@gmail.com>
2022-07-01 07:30:55 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+int i;
+i = 48;
+while  (i < 58)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
