2b74f42a196ef48b426880e85ca054888a2b3b3d
Chetachi <maduchetat231@gmail.com>
2022-07-05 09:52:24 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_sign - checks for integers sign
+ * @n: integer we are checking
+ * return: 1, 0 or -1
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
+}
