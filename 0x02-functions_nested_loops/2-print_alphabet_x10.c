dd9ded4511e957eb7b74a92679a52439eb119a21
Chetachi <maduchetat231@gmail.com>
2022-07-05 09:11:18 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ * return: void
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
