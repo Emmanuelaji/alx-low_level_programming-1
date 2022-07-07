0e6bb8ccd0617e65acb16744456d5b38ec401caf
Chetachi <maduchetat231@gmail.com>
2022-07-07 08:32:10 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,10 @@
  */
 void print_numbers(void)
 {
-char r;
-for (r = '0'; r <= '9'; r++)
+int r;
+for (r = 0; r <= 9; r++)
 {
 _putchar(r);
-_putchar('\n');                                                                   
 }
+_putchar('\n');
 }
