ae04d917f82012600988cfbbaeeb7104f5af6528
Chetachi <maduchetat231@gmail.com>
2022-07-12 05:22:58 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i))
+	while (str[i] != '\0')
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i += 1;
 	}
 	_putchar('\n');
 }
