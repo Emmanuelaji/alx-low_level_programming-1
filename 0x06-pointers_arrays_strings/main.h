650574fa2ca7427729473779abc11fc87032fc52
Chetachi <maduchetat231@gmail.com>
2022-07-15 01:37:04 -0700

    Magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef _main_H_
+#define _main_H_
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -9,3 +12,5 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+
+#endif
