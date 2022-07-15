2bf301d892ab05480ba5f0d644ee63ff7e40ca49
Chetachi <maduchetat231@gmail.com>
2022-07-14 23:07:05 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,16 +10,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n && *(src + i))
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
+	for (i = 0; src[i] != '\0'; i++)
+		if (i < n)
+			dest[i] = src[i];
 	while (i < n)
-	{
-		*(dest + i) = '\0';
-		i++;
-	
-		return (dest);
+		dest[i++] = '\0';
+	return (dest);
 }
