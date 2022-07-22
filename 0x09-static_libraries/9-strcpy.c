81227a61002fa11b8c86b23fddd2a5c6c2fc9df7
Chetachi Maduabuchi <106966796+sarah2341@users.noreply.github.com>
2022-07-22 08:50:16 +0100

    Update 9-strcpy.c

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcpy - copies string pointed to by src to buffer pointed to by dest
+ * @src: source to copy
+ * @dest: destination of copy
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Priv
