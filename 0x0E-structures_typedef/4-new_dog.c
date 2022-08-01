593115a61073c880fd482ae726e1b24d365c6961
Chetachi <maduchetat231@gmail.com>
2022-08-01 13:53:22 -0700

    task 2

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include "dog.h"
-
 /**
  * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- *
  * Return: pointer to new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
