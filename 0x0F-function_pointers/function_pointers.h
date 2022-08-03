d9b77c4fb05ca89170c456bd70755e1a484365e2
Chetachi <maduchetat231@gmail.com>
2022-08-03 05:01:06 -0700

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,5 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
-
-#include <stddef.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
