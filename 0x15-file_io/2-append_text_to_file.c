c0aceb9d955128de5999cc1699faea0c70ad9ee1
Chetachi <maduchetat231@gmail.com>
2022-08-22 13:17:38 -0700

    Correction 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,46 +1,29 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
 
 /**
- * _strlen - finds the length of a string
- * @str: pointer to the string
- * Return: length of the string
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file
+ * @text_content: the NULL terminated string to add at end of file
+ * Return: 1 on success, -1 on failure
  */
-
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
- * append_text_to_file - appends a text at the end of a file.
- * @filename: name of the file
- * @text_content: NULL terminated string to add at the end of the file
- * Return: 1 on success and -1 on failure
-*/
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int i;
-	ssize_t len;
+	int file, fwrite, i;
 
 	if (filename == NULL)
 		return (-1);
-	i = open(filename, O_WRONLY | O_APPEND);
-	if (i == -1)
+	file = open(filename, O_APPEND | O_RDWR, 0664);
+	if (file == -1)
 		return (-1);
 	if (text_content != NULL)
-		len = write(i, text_content, _strlen(text_content));
-	close(i);
-	if (len == -1)
-		return (-1);
+	{
+		for (i = 0; text_content[i]; i++)
+			;
+		fwrite = write(file, text_content, i);
+		if (fwrite == -1)
+			return (-1);
+	}
+	close(file);
 	return (1);
 }
